cc44e109053689e86c27b46c86aa32994b6ba1ee
ArcaneSand <time-2546@hotmail.com>
2026-03-16 10:58:52 +0700

    fix production deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-<<<<<<< HEAD
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
-=======
-import { Mona_Sans } from "next/font/google";
->>>>>>> 1a1d0b5c454306c77806376f80a5da067059a0f1
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
